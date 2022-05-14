532f640e5347c2beb3161f57b84615016cd897af
unknown <salunkeakshad@gmail.com>
2022-05-14 12:08:01 +0530

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: "mongodb+srv://Akshad_14:Akshad07@onlinedatingapp.o6wya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    GoogleClientID: "534076297739-su2qo9qihbki3m28lotaf2u2r0mbv3if.apps.googleusercontent.com",
+    GoogleClientSecret: "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"
+}
