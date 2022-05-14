7ecb6e6e9c90cd25b886ea850dd68c8362b263b2
unknown <salunkeakshad@gmail.com>
2022-05-14 12:32:04 +0530

    config keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: "mongodb+srv://Akshad_14:Akshad07@onlinedatingapp.o6wya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    GoogleClientID: "534076297739-su2qo9qihbki3m28lotaf2u2r0mbv3if.apps.googleusercontent.com",
-    GoogleClientSecret: "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"
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
