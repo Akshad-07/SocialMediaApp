62028a05db55a769401440387ffd83b7839c0d50
unknown <salunkeakshad@gmail.com>
2022-05-16 12:14:21 +0530

    prod

---
+++ config/keys-prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    FacebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    FacebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    InstagramClientID: process.env.INSTAGRAM_CLIENT_ID,
+    InstagramClientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+}
